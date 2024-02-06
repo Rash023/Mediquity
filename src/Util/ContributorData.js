207e44abe09fde564b15fb2121a7fa528b3ff2a2
Rash023 <rashidmazhar0507@gmail.com>
2024-02-06 11:04:52 +0530

    added contributors

--- src/Util/ContributorData.js
+++ src/Util/ContributorData.js
@@ -1,37 +1,37 @@
 export const ContributorData = [
-    {
-        name: 'Ayan Bera',
-        role: 'BackEnd',
-        src: 'https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
-        linkedIn: '',
-        github: ''
-    },
-    {
-        name: 'Hindol Roy',
-        role: 'BackEnd & ML - Ops',
-        src: 'https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
-        linkedIn: '',
-        github: ''
-    },
-    {
-        name: 'Rashid Mazhar',
-        role: 'Ml - Ops',
-        src: 'https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
-        linkedIn: '',
-        github: ''
-    },
-    {
-        name: 'Aritra Saha',
-        role: 'UI / UX',
-        src: 'https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
-        linkedIn: '',
-        github: ''
-    },
-    {
-        name: 'Abhrajit Das',
-        role: 'Ml - Ops',
-        src: 'https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale',
-        linkedIn: '',
-        github: ''
-    },
-]
+  {
+    name: "Ayan Bera",
+    role: "BackEnd",
+    src: "https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    linkedIn: "https://www.linkedin.com/in/ayan-bera-436517261/",
+    github: "https://github.com/GitAyan007",
+  },
+  {
+    name: "Hindol Roy",
+    role: "BackEnd & ML - Ops",
+    src: "https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    linkedIn: "https://www.linkedin.com/in/hindol-roy/",
+    github: "https://github.com/hRJ06",
+  },
+  {
+    name: "Rashid Mazhar",
+    role: "BackEnd & Ml - Ops",
+    src: "https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    linkedIn: "https://www.linkedin.com/in/rashid-mazhar-73147b226/",
+    github: "https://github.com/Rash023",
+  },
+  {
+    name: "Aritra Saha",
+    role: "UI / UX",
+    src: "https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    linkedIn: "https://www.linkedin.com/in/aritra-saha-cs/",
+    github: "",
+  },
+  {
+    name: "Abhrajit Das",
+    role: "Ml - Ops",
+    src: "https://static01.nyt.com/images/2024/01/31/multimedia/31tesla-musk-vgfc/31tesla-musk-vgfc-articleLarge.jpg?quality=75&auto=webp&disable=upscale",
+    linkedIn: "",
+    github: "",
+  },
+];
