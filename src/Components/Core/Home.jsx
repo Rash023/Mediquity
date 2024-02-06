aecd99497ad0e5c9498493be84298431428d7cd0
hRJ06 <hindolroy.2306@gmail.com>
2024-02-07 01:20:33 +0530

    Added the Config for Sparkles and Spotlight Component

--- src/Components/Core/Home.jsx
+++ src/Components/Core/Home.jsx
@@ -17,6 +17,7 @@ import ProvideCard from './ProvideCard'
 import ScanPredictionCard from './ScanPredictionCard'
 import { GeminiEffect } from './GeminiEffect'
 import Blog from './Blog'
+import SpotlightHero from './SpotLightHero'
 
 
 
@@ -39,8 +40,8 @@ export const Home = () => {
       <Navbar className='' />
       <div>
         <div className="h-fit-content  dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex flex-col items-center justify-center font-sans">
-
           <HeroSection />
+         
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <ServiceCard />
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,25 @@ const {
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    fontFamily: {
-      sans: ['Google Sans Display', 'Arial', 'sans-serif'],
+    extend: {
+      fontFamily: {
+        sans: ['Google Sans Display', 'Arial', 'sans-serif'],
+      },
+      animation: {
+        spotlight: "spotlight 2s ease .75s 1 forwards",
+      },
+      keyframes: {
+        spotlight: {
+          "0%": {
+            opacity: 0,
+            transform: "translate(-72%, -62%) scale(0.5)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+      },
     },
   },
   plugins: [
