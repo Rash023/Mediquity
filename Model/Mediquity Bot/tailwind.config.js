2771fd763d8a4b3b05726fcb1e11ba4bc71f50f3
hRJ06 <hindolroy.2306@gmail.com>
2024-02-09 01:22:54 +0530

    Updated folder structure

---
+++ Model/Mediquity Bot/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors: {
+        gemLight: "#e5f5e0",
+        gemRegular: "#a1d99b",
+        gemDeep: "#31a354",
+      },
+    },
+  },
+  plugins: [],
+};
