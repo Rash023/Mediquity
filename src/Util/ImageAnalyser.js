21fae35e10d6f4cfef335f5e69c3f1b5cbf81fb6
hRJ06 <hindolroy.2306@gmail.com>
2024-02-12 22:08:11 +0530

    Added Disease Prediction through Image Analysis

---
+++ src/Util/ImageAnalyser.js
@@ -0,0 +1,36 @@
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+const genAI = new GoogleGenerativeAI("AIzaSyB5v4JcdsO0gLlgPhSkPD6CZYefcWY7aHk");
+
+async function fetchImage(url) {
+  const fetchModule = await import('node-fetch');
+  const fetch = fetchModule.default;
+
+  const response = await fetch(url);
+  const buffer = await response.buffer();
+  return {
+    inlineData: {
+      data: buffer.toString("base64"),
+      mimeType: response.headers.get("content-type")
+    }
+  };
+}
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
+
+  const prompt = "Explain about this disease.";
+
+  const imageLinks = [
+    "https://img.medscapestatic.com/pi/meds/ckb/33/29533tn.jpg"
+  ];
+
+  const imageParts = await Promise.all(imageLinks.map(fetchImage));
+
+  const result = await model.generateContent([prompt, ...imageParts]);
+  const response = await result.response;
+  const text = await response.text();
+  console.log(text);
+}
+
+run();
