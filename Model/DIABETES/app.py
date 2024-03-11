cc0be88c258e12e94c8972c489277538256786f6
hRJ06 <hindolroy.2306@gmail.com>
2024-03-11 20:34:30 +0530

    Added React support for JSON input

--- Model/DIABETES/app.py
+++ Model/DIABETES/app.py
@@ -1,43 +1,41 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 import pickle as pkl
 import numpy as np
 
 app = Flask(__name__)
 
-
 with open('D:\ML\DISEASE PREDICTION\DIABETES\Model\diabetesPredictionModel.pkl', 'rb') as file:
     model = pkl.load(file)
 
-@app.route('/')
-def home():
-    return render_template('index.html', final_result='')
-
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = {
-        'age': int(request.form['age']),
-        'gender': request.form['gender'],
-        'hypertension': int(request.form['hypertension']),
-        'heart_disease': int(request.form['heart_disease']),
-        'smoking_history': int(request.form['smoking_history']),
-        'bmi': float(request.form['bmi']),
-        'HbA1c_level': float(request.form['HbA1c_level']),
-        'blood_glucose_level': int(request.form['blood_glucose_level'])
-    }
+    data = request.json
     
-    
+    # Convert gender to binary
     data['gender'] = 1 if data['gender'] == 'Male' else 0
     
+    # Prepare data for prediction
+    input_data = np.array([[
+        data['age'],
+        data['gender'],
+        data['hypertension'],
+        data['heart_disease'],
+        data['smoking_history'],
+        data['bmi'],
+        data['HbA1c_level'],
+        data['blood_glucose_level']
+    ]])
     
-    prediction = model.predict(np.array(list(data.values())).reshape(1, -1))
+    # Make prediction
+    prediction = model.predict(input_data)
     
-    
+    # Return prediction
     if prediction[0] == 0:
         result = "Non-diabetic"
     else:
         result = "Diabetic"
     
-    return render_template('index.html', final_result=result)
+    return jsonify({'result': result})
 
 if __name__ == '__main__':
     app.run(debug=True)
