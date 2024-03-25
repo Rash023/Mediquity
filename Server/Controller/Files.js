0e4315b2a23922e8f8215eedbb4fb360c52e3e12
Rash023 <rashidmazhar0507@gmail.com>
2024-03-25 15:45:30 +0530

    Added View and Search Files

--- Server/Controller/Files.js
+++ Server/Controller/Files.js
@@ -1,6 +1,7 @@
 const { cloudinaryConnect } = require("../Configuration/Cloudinary");
 const File = require("../Model/Files");
 const User = require("../Model/User");
+const jwt = require("jsonwebtoken");
 
 const cloudinary = require("cloudinary").v2;
 
@@ -82,7 +83,18 @@ exports.fileuploader = async (req, res) => {
 
 exports.SearchFile = async (req, res) => {
   try {
-    const { userId, Name } = req.body;
+    const token = req.body.token;
+    const { name } = req.body;
+
+    if (!token) {
+      return res.status.json({
+        success: false,
+        message: "Token is missing",
+      });
+    }
+
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.id;
 
     const user = await File.findOne({ userId });
 
@@ -92,7 +104,7 @@ exports.SearchFile = async (req, res) => {
         message: "User not found",
       });
     }
-    const foundFile = await user.files.find((file) => file.filename === Name);
+    const foundFile = await user.files.find((file) => file.filename === name);
 
     if (!foundFile) {
       return res.status(200).json({
@@ -102,14 +114,45 @@ exports.SearchFile = async (req, res) => {
     } else {
       return res.status(200).json({
         success: true,
+        data: foundFile,
         message: "File found",
-        foundFile,
       });
     }
   } catch (error) {
     return res.status(500).json({
       success: false,
       message: "Internal Server Error",
+    });
+  }
+};
+
+//handler to get all the files of the user
+
+exports.getFiles = async (req, res) => {
+  try {
+    const token = req.body.token;
+
+    if (!token) {
+      return res.status.json({
+        success: false,
+        message: "Token not found",
+      });
+    }
+
+    let decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decodedToken.id;
+
+    const Data = await File.findOne({ userId: userId });
+
+    return res.status(200).json({
+      success: true,
+      data: Data,
+      message: "Files Fetched Succesfully",
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Interval Server Error",
     });
   }
 };
