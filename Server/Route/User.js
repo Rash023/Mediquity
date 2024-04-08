7d247c63e17035036caa98574a5200480d77ef06
Rash023 <rashidmazhar0507@gmail.com>
2024-04-08 16:37:06 +0530

    Vercel Config

--- Server/Route/User.js
+++ Server/Route/User.js
@@ -6,20 +6,22 @@ const { getSlots } = require("../Controller/Slots");
 const {
   createAppointment,
   getAppointments,
+  cancelAppointment,
 } = require("../Controller/Appointments");
 
 const express = require("express");
 const router = express.Router();
 
-router.get("/",userDetail);
-router.post("/login", login); 
-router.post("/signup", Signup); 
-router.post("/medication", createMedication); 
-router.post("/upload", fileuploader); 
-router.post("/search", SearchFile); 
-router.get("/viewFiles", getFiles); 
-router.post("/contactUs", contactUsController); 
-router.post("/bookAppointment", createAppointment); 
-router.get("/getAppointments", getAppointments); 
-router.get("/getSlots", getSlots); 
+router.get("/", userDetail);
+router.post("/login", login);
+router.post("/signup", Signup);
+router.post("/medication", createMedication);
+router.post("/upload", fileuploader);
+router.post("/search", SearchFile);
+router.get("/viewFiles", getFiles);
+router.post("/contactUs", contactUsController);
+router.post("/bookAppointment", createAppointment);
+router.get("/getAppointments", getAppointments);
+router.get("/cancelAppointment", cancelAppointment);
+router.get("/getSlots", getSlots);
 module.exports = router;
