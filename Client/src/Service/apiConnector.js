1c319e5f8fd8bccc8470d6545d9371f8fe094582
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2024-06-02 11:35:15 +0530

    Update apiConnector.js

--- Client/src/Service/apiConnector.js
+++ Client/src/Service/apiConnector.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const axiosInstance = axios.create({});
+export const axiosInstance = axios.create({
+   baseURL: process.env.REACT_APP_BASE_URL,
+});
 
 export const apiConnector = (method, url, bodyData, headers, params) => {
   return axiosInstance({
