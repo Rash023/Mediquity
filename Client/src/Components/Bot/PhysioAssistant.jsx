8331898366154a552ef53b33fcd2d7c3199b9c92
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2025-03-28 14:13:52 +0530

    Update PhysioAssistant.jsx model

--- Client/src/Components/Bot/PhysioAssistant.jsx
+++ Client/src/Components/Bot/PhysioAssistant.jsx
@@ -9,7 +9,7 @@ import { useSelector } from "react-redux";
 
 const API_KEY = process.env.REACT_APP_GEMINI_API_KEY
 const genAI = new GoogleGenerativeAI(`${API_KEY}`);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const md = new Markdown();
 
 const PhysioAssistant = () => {
