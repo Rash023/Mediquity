e9f26f9f4ccc58ea867844c41c9a28881ecde26e
Rashid Mazhar <91797681+Rash023@users.noreply.github.com>
2025-03-28 14:43:12 +0530

    update: Updated model type and syntax PersonalTherapist.jsx

--- Client/src/Components/Bot/PersonalTherapist.jsx
+++ Client/src/Components/Bot/PersonalTherapist.jsx
@@ -9,11 +9,11 @@ import { useSelector } from "react-redux";
 
 const API_KEY = process.env.REACT_APP_GEMINI_API_KEY
 const genAI = new GoogleGenerativeAI(`${API_KEY}`);
-const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 const md = new Markdown();
 
 const PersonalTherapist = () => {
-  const { token } = useSelector((state) => state.auth);
+  const { token } = useSelector((state) => state.auth);s
   const { user } = useSelector((state) => state.auth);
   const [newMessage, setNewMessage] = useState("");
   const navigate = useNavigate();
@@ -155,11 +155,11 @@ const PersonalTherapist = () => {
                       }`}
                   >
                     {message.role === "user" ? (
-                      <img
-                        src={`https://api.dicebear.com/8.x/initials/svg?seed=${user.replace(' ', '%20')}`}
-                        alt="User Avatar"
-                        className="w-10 h-10 md:w-12 md:h-12 rounded-full"
-                      />
+                     <img
+                      src={`https://api.dicebear.com/8.x/initials/svg?seed=${user?.name ? user.name.replace(' ', '%20') : 'Anonymous'}`}
+                      alt="User Avatar"
+                      className="w-10 h-10 md:w-12 md:h-12 rounded-full"
+                    />
                     ) : (
                       <img
                         src="https://i.pinimg.com/originals/0c/67/5a/0c675a8e1061478d2b7b21b330093444.gif"
